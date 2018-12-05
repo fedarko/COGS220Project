e1f11f2e3d23ab746c27f18617a1cd69c321b81f
sid-dinesh94 <f2012519@goa.bits-pilani.ac.in>
2018-12-04 18:57:28 -0800

    Read json data, add tooltips

--- code.js
+++ code.js
@@ -15,7 +15,7 @@ function makeTooltip(e) {
   tooltip.html(
     `
     <div class="content">
-      ${e.date.toString().substring(17, 21)}
+      ${e.tooltip.toString()}
     </div>
     `
   ).style('left', `${d3.event.pageX - 30}px`).style('top', `${d3.event.pageY + 20}px`);
@@ -46,7 +46,7 @@ function objLength(obj) {
   return Object.keys(obj).length - 1;
 }
 
-function unifyStuff(hist) {
+/*function unifyStuff(hist) {
 
    var outputData = [];
    for (var t = 0; t < hist.length; t++) {
@@ -65,7 +65,7 @@ function unifyStuff(hist) {
    }
    return outputData;
 }
-
+*/
 function randn_bm(scaleFactor) {
     var u = 0, v = 0;
     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
@@ -114,50 +114,139 @@ function replicateNewsZoom(e) {
     //d(d3.select("#tweetsChart").datum(tweetsData).data());
 }
 
-const tweetsHistCSV = ",0,1,2,3,4,5,6\n0,11.0,28.0,24.0,37.0,27,33.0,59.0\n1,24.0,23.0,14.0,17.0,23,11.0,35.0\n2,27.0,21.0,38.0,19.0,25,27.0,67.0\n3,26.0,15.0,38.0,19.0,37,26.0,86.0\n4,13.0,19.0,22.0,14.0,22,14.0,25.0\n5,26.0,31.0,30.0,13.0,25,29.0,77.0\n6,26.0,30.0,40.0,31.0,32,41.0,74.0\n7,11.0,27.0,23.0,26.0,26,49.0,65.0\n8,3.0,2.0,6.0,,1,3.0,164.0\n9,13.0,24.0,29.0,8.0,13,11.0,32.0\n10,25.0,8.0,34.0,18.0,10,6.0,25.0\n11,13.0,23.0,35.0,26.0,27,29.0,65.0\n12,,,,33.0,58,,\n13,22.0,27.0,27.0,32.0,34,32.0,58.0\n14,15.0,21.0,34.0,14.0,28,32.0,44.0\n15,17.0,25.0,31.0,35.0,25,30.0,46.0\n16,11.0,16.0,45.0,19.0,31,29.0,40.0\n17,11.0,22.0,35.0,21.0,51,34.0,75.0\n18,8.0,7.0,10.0,9.0,30,26.0,26.0\n19,4.0,12.0,40.0,23.0,16,39.0,27.0\n20,1.0,6.0,5.0,6.0,1,1.0,5.0\n21,15.0,30.0,33.0,26.0,38,36.0,57.0\n22,55.0,38.0,18.0,16.0,11,11.0,19.0\n23,30.0,18.0,19.0,17.0,29,18.0,64.0\n24,28.0,21.0,41.0,35.0,42,35.0,89.0";
-var tweetsHist = d3.csvParse(tweetsHistCSV);
 
-const newsHistCSV = ",0,1,2,3,4,5,6\n0,510,656,615,652,641,378,466\n1,340,540,587,666,442,202,212\n2,526,562,659,694,633,593,401\n3,655,600,532,572,675,511,564\n4,333,284,399,390,506,458,349\n5,456,533,466,609,526,74,111\n6,451,542,607,675,645,342,371\n7,452,513,428,324,390,253,255\n8,554,439,309,448,356,190,505\n9,369,403,540,397,394,275,290\n10,1355,1081,661,487,326,194,157\n11,424,667,500,583,560,251,380\n12,372,496,478,559,513,415,488\n13,156,170,124,187,117,53,69\n14,626,763,571,558,553,160,161\n15,280,422,300,325,219,59,102\n16,204,297,287,340,276,156,155\n17,372,399,524,529,463,250,242\n18,215,239,194,234,307,296,330\n19,393,381,235,354,339,344,368\n20,245,233,267,252,390,314,297\n21,96,190,159,154,150,37,9\n22,551,679,547,673,510,305,393\n23,275,337,356,398,456,236,370\n24,482,482,543,569,580,347,159";
-var newsHist = d3.csvParse(newsHistCSV);
-
-var tweetsColors = [];
-var newsColors = [];
-for (ci = 0; ci < tweetsHist.length; ci++) {
-    tweetsColors.push(colorLine());
-}
-for (var ci = 0; ci < newsHist.length; ci++) {
-    newsColors.push(colorLine());
-}
-
-var tweetsConfig = {
-  range: {
-    start: new Date('10/01/2017 6:55:11 PM'),
-    end: new Date('10/09/2017 7:15:11 PM')
-  },
-  //metaballs: {
-  //  blurDeviation: 25
-  //},
-  drop: {
-    date: d => d.date,
-    // color: data => colorDrop(data),
-    onMouseOver: makeTooltip,
-    onMouseOut: rmTooltip
-  },
-  zoom: {
-    onZoom: replicateTweetsZoom
-  },
-  line: {
-    color: (data, index) => tweetsColors[index]
+function createEventsFromTweets(jsonData){
+  console.log('Entered json processor');
+  var outputData = [];
+  //console.log(jsonData)
+  //console.log(jsonData.length());
+  for (var group in jsonData){
+    console.log('Processing group ' + group + 'with ' + jsonData[group].length);
+    var group_events = jsonData[group];
+    var topic_data = [];
+    for (var event in group_events){
+      topic_data.push({date: new Date(group_events[event]['date']), 
+                        tooltip: group_events[event]['content']});
+    }
+    var dataInstance = {name: group, data: topic_data};
+    outputData.push(dataInstance);
   }
-};
+  return outputData;
+}
 
-var counter = 0;
-var y = 16777215
-var first_date = 0;
+function createEventsFromNews(jsonData){
+  var outputData = [];
+  //console.log(jsonData)
+  //console.log(jsonData.length());
+  for (var group in jsonData){
+    console.log('Processing news group ' + group + 'with ' + jsonData[group].length);
+    var group_events = jsonData[group];
+    var topic_data = [];
+    for (var event in group_events){
+      var noise = getRandomNoise();
+      var temp = new Date(group_events[event]['date']);
+      // console.log(temp)
+      temp.setSeconds(temp.getSeconds() + noise + 60*60*8);
+      topic_data.push({date: temp, 
+                      tooltip: group_events[event]['content']});
+    }
+
+    var dataInstance = {name: group, data: topic_data};
+    
+    outputData.push(dataInstance);
+    
+  }
+  return outputData;
+}
+
+
+var rowColors = [];
+for (ci = 0; ci < 25; ci++) {
+    rowColors.push(colorLine());
+}
+
+
+
+  var newsConfig = {
+    range: {
+      start: new Date('10/01/2017 6:55:11 PM'),
+      end: new Date('10/09/2017 7:15:11 PM')
+    },
+    //metaballs: {
+    //  blurDeviation: 25
+    //},
+    drop: {
+      date: d => d.date,
+      onMouseOver: makeTooltip,
+      onMouseOut: rmTooltip
+    },
+    zoom: {
+      onZoom: replicateNewsZoom
+    },
+    line: {
+      color: (data, index) => rowColors[index],
+    }
+  };
+
+  var tweetsConfig = {
+    range: {
+      start: new Date('10/01/2017 6:55:11 PM'),
+      end: new Date('10/09/2017 7:15:11 PM')
+    },
+    //metaballs: {
+    //  blurDeviation: 25
+    //},
+    drop: {
+      date: d => d.date,
+      // color: data => colorDrop(data),
+      onMouseOver: makeTooltip,
+      onMouseOut: rmTooltip
+    },
+    zoom: {
+      onZoom: replicateTweetsZoom
+    },
+    line: {
+      color: (data, index) => rowColors[index],
+    }
+  };
+
+  var counter = 0;
+  var y = 16777215
+  var first_date = 0;
+
+var tweet_file ="https://raw.githubusercontent.com/sid-dinesh94/sid-dinesh94.github.io/master/projects/textIntegration/news_sourced/tweets.json";
+var news_file ="https://raw.githubusercontent.com/sid-dinesh94/sid-dinesh94.github.io/master/projects/textIntegration/news_sourced/news.json";
+
+var tweetsData;
+var data_copy;
+d3.queue()
+  .defer(d3.json, tweet_file)
+  .defer(d3.json, news_file)
+  .await(render);
+
+function render(error, tweet_json, news_json) {
+  if(error){
+    console.log(error)
+  }
+  data_copy = news_json;
+  var tweetsData = createEventsFromTweets(tweet_json);
+  var newsData = createEventsFromNews(news_json);
+  console.log(newsData)
+
+  //var tweetsData = unifyStuff(tweetsHist);
+  //var newsData = unifyStuff(newsHist);
+
+  createChart("tweets", tweetsData);
+  createChart("news", newsData);
+};
+//console.log(tweets_data)
+//tweetData = createEventsFromTweets(tweet_data);
+
+
 
 function colorDrop(data){
   counter = counter + 1;
-  console.log("Counter: ", counter);
+  //console.log("Counter: ", counter);
   var valuee = data.date.getTime();
   if (counter == 1) {
     first_date = valuee;
@@ -166,10 +255,10 @@ function colorDrop(data){
     valuee =  Math.abs(first_date - valuee)
   }
   var z = Math.round(valuee % y);
-  console.log("Value: ", valuee.toString(16));
-  console.log("Quotient: ", z.toString(16));
+  //console.log("Value: ", valuee.toString(16));
+  //console.log("Quotient: ", z.toString(16));
   var col = "#".concat(z.toString(16));
-  console.log("Color:", col)
+  //console.log("Color:", col)
   return col;
   // if (counter > 0 && counter <= 100) {return 'red';}
   // if (counter > 100 && counter < 200) {return 'blue';}
@@ -197,30 +286,10 @@ function colorLine(data){
   return col;
 }
 
-var newsConfig = {
-  range: {
-    start: new Date('10/01/2017 6:55:11 PM'),
-    end: new Date('10/09/2017 7:15:11 PM')
-  },
-  //metaballs: {
-  //  blurDeviation: 25
-  //},
-  drop: {
-    date: d => d.date,
-    onMouseOver: makeTooltip,
-    onMouseOut: rmTooltip
-  },
-  zoom: {
-    onZoom: replicateNewsZoom
-  },
-  line: {
-    color: (data, index) => newsColors[index]
-  }
-};
 
 var tweetsChart, newsChart;
 
-function createChart(chartType, otherScale) {
+function createChart(chartType, data, otherScale) {
     var chart;
     var data;
     var divID = "#";
@@ -231,12 +300,10 @@ function createChart(chartType, otherScale) {
     }
     if (chartType === "news") {
         newsChart = eventDrops(config);
-        data = unifyStuff(newsHist);
         divID += "newsChart";
         chart = newsChart;
     } else {
         tweetsChart = eventDrops(config);
-        data = unifyStuff(tweetsHist);
         divID += "tweetsChart";
         chart = tweetsChart;
     }
@@ -248,13 +315,18 @@ function replaceChart(chartType) {
     if (chartType === "news") {
         divID = "newsChart";
         newScale = tweetsChart.scale().domain();
+        data = newsChart.filteredData()
+        console.log(data)
     } else {
         divID = "tweetsChart";
         newScale = newsChart.scale().domain();
+        data = tweetsChart.filteredData();
+        console.log(data);
     }
+    console.log(data)
     d3.select("#" + divID + " *").remove();
-    createChart(chartType, newScale);
+    createChart(chartType, data, newScale);
 }
 
-createChart("tweets");
-createChart("news");
+
+
